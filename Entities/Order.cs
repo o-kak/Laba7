cdbbe1b56fb2dfd3d998e3c96c765a9c47371acc
evelson <euroset882605@gmail/com>
2025-07-01 17:13:03 +0700

    Добавлена библиотека классов, содержащая сущности

---
+++ Entities/Order.cs
@@ -0,0 +1,26 @@
+﻿namespace Entities
+{
+    public enum OrderTypes
+    {
+        TakeOut,
+        Delivery
+    }
+
+    public enum OrderStates
+    {
+        Created,
+        Processing,
+        AlmostReady,
+        Ready,
+        Given,
+        Closed
+    }
+    public class Order : IDomainObject
+    {
+        public string ID { get; set; }
+        public OrderTypes Type { get; set; }
+        public string WaiterID { get; set; }
+        public OrderStates State { get; set; }
+        public bool IsPaidFor { get; set; }
+    }
+}
